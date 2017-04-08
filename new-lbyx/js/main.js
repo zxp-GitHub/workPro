fb4d445ed03495c0c711878bc88abf459473452a
zxp-GitHub <572402296@qq.com>
2017-04-08 17:11:40 +0800

    new-lbyx

--- new-lbyx/js/main.js
+++ new-lbyx/js/main.js
@@ -3,6 +3,11 @@
         $(this).addClass('active').siblings().removeClass('active');
         $('.big-ul>li').hide().eq($(this).index()).show();
         //$('.big-ul li')会报错，是指所有的li
-    })
+   });
+/*S all-goods*/
+	$(".all-goods-left-ul>li").tap(function  () {
+//		alert($(this).index()); /*不会console一下*/
+		$(this).css({"background-color":" #fff","color":"red"}).siblings().css({"background-color":"#E5E5E5","color":"#000"})
+	})
+/*E all-goods*/
  });
- 
