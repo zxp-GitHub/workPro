8c7df14ab8bdbfbf74ec7f1d78805b1e9a6c1d8c
zxp-GitHub <572402296@qq.com>
2017-04-21 18:02:51 +0800

    new-lbyx

--- new-lbyx/js/main.js
+++ new-lbyx/js/main.js
@@ -12,12 +12,45 @@
 	$('.all-goods-left-ul>li').tap(function(){
         $(this).addClass('active').siblings().removeClass('active');
         $('.all-goods-list-ul>li').hide().eq($(this).index()).show()
-    })
+   });
 /*E all-goods*/
 
-
-
 /*S product-details*/
- 
+ $(".pro-detail-three-num").html(1);
+$(".pro-detail-flour-num").html(1);
+$(".pro-detail-three-p").find(".icon-jianhao1").on('click',function () {
+	if (parseInt($(".pro-detail-three-num").html())>1){
+		$(".pro-detail-three-num").html(parseInt($(".pro-detail-three-num").html())-1);
+     	$(".pro-detail-four-num").html($(".pro-detail-three-num").html())
+	} else{
+	 return;
+	}
+   if (parseInt($(".pro-detail-three-num").html())==1){
+   	$(".icon-jianhao11").css("color","#ccc");
+   }
+});
+
+$(".pro-detail-three-p").find(".icon-jiahao2").on('click',function () {
+	if (parseInt($(".pro-detail-three-num").html())==1){
+		$(".icon-jianhao11").css("color","red");
+	}
+	  $(".pro-detail-three-num").html(parseInt($(".pro-detail-three-num").html())+1);
+	  $(".pro-detail-four-num").html($(".pro-detail-three-num").html());
+});
+
+//轮播图
+ var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+//  longSwipes : false,
+    loop: true,
+//  onlyExternal : false,
+    autoplay : 2000,
+//  autoplayDisableOnInteraction : false,
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+  })        
 /*E product-details*/
+/*S confirm-order*/
+
+/*E confirm-order*/
  });
