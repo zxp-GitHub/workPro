89ec1e7eee87a81c58d36651e78023cdd40a8479
zxp-GitHub <572402296@qq.com>
2017-05-08 17:17:31 +0800

    new-lbyx

--- new-lbyx/js/main.js
+++ new-lbyx/js/main.js
@@ -1,4 +1,4 @@
- $(function  () {
+Zepto(function() {
  $('.footer-ul li').on('tap',function(){
         $(this).addClass('active').siblings().removeClass('active');
         $('.big-ul>li').hide().eq($(this).index()).show();
@@ -27,41 +27,60 @@ $(".pro-detail-three-p").find(".icon-jianhao1").on('click',function () {
 	}
    if (parseInt($(".pro-detail-three-num").html())==1){
    	$(".icon-jianhao11").css("color","#ccc");
+   	$(".sell-out").show();
    }
 });
 
 $(".pro-detail-three-p").find(".icon-jiahao2").on('click',function () {
 	if (parseInt($(".pro-detail-three-num").html())==1){
 		$(".icon-jianhao11").css("color","red");
+		$(".sell-out").hide();
 	}
 	  $(".pro-detail-three-num").html(parseInt($(".pro-detail-three-num").html())+1);
 	  $(".pro-detail-four-num").html($(".pro-detail-three-num").html());
 });
 
-//轮播图
- var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal',
-    longSwipes : false,
-    loop: true,
-//  onlyExternal : false,
-    autoplay : 2000,
-    autoplayDisableOnInteraction : false,//禁止自动播放的作用
-    // 如果需要分页器
-    pagination: '.swiper-pagination',
-  });
-  $(".add-to-car").tap(function(){
+$(".add-to-car").tap(function(){
   		$('.ok-add-cart-mask').fadeIn();
   		
 	setTimeout(function  () {
 		$('.ok-add-cart-mask').fadeOut()
-//		$('.ok-add-cart-mask').hide();
-//		$('.ok-add-cart-mask').animate({ opacity: 0}, 500, 'ease-out')
 	},1300)
 	});
     $(".right-icon-tap").tap(function(){
   	$(".ok-add-cart-mask").hide()
   });
   
+  //轮播图的改变
+  //一定要注意，必须要从home页跳转到商品详情页，banner才不会报错。
+		$.ajax({//获取json数据必写哦！！！
+			type:"get",
+			url:"json/lbyx.json",
+			dataType:"json",
+			success:function(data){//consol一下是很重要的！！！！！！
+				//假如一页获取m条
+				     var homeNums = window.sessionStorage.getItem("homeNum")||"";
+//				     console.log(data.homeList[homeNums]);
+					 var html= "<div class=\"swiper-slide\"><img src=\""+data.homeList[homeNums].banner[0]+"\"/></div>";
+						 html+="<div class=\"swiper-slide\"><img src=\""+data.homeList[homeNums].banner[1]+"\"/></div>";
+						 html+="<div class=\"swiper-slide\"><img src=\""+data.homeList[homeNums].banner[2]+"\"/></div>";
+						$(".swiper-wrapper").append(html);//通过不断地加载，但是点击之后ul又会清空来实现分页的效果
+						$(".pro-detail-two-goods-info").html(data.homeList[homeNums].name);
+						$(".pro-detail-two-price").html(data.homeList[homeNums].price)
+						
+						    //轮播图
+							var mySwiper = new Swiper ('.swiper-container', {
+						    direction: 'horizontal',
+						    longSwipes : false,
+						    loop: true,
+						//  onlyExternal : false,
+						    autoplay : 2000,
+						    autoplayDisableOnInteraction : false,//禁止自动播放的作用
+						    // 如果需要分页器
+						    pagination: '.swiper-pagination',
+ 							});
+				}
+		});
 /*E product-details*/
 /*S confirm-order*/
 
@@ -69,7 +88,7 @@ $(".pro-detail-three-p").find(".icon-jiahao2").on('click',function () {
 /*S index*/
 $(".shopping-car-change-price>.icon-jiahao2").tap(function(){
 	var aaa= parseInt($(this).parent().find("span").html());
-	console.log(aaa);
+//	console.log(aaa);
 	$(this).parent().find("span").html(aaa+1);
 	if (aaa==0) {
 		console.log($(this).css("color"));
@@ -78,7 +97,7 @@ $(".shopping-car-change-price>.icon-jiahao2").tap(function(){
 });
 $(".shopping-car-change-price>.icon-jianhao1").tap(function(){
 	var aaa= parseInt($(this).parent().find("span").html());
-	console.log(aaa)
+//	console.log(aaa)
 	if (aaa>0) {
 		$(this).parent().find("span").html(aaa-1);
 	} else{
@@ -89,6 +108,40 @@ $(".shopping-car-change-price>.icon-jianhao1").tap(function(){
 	}
 });
 
-
+//商品列表ajax
+var ajaxLi =  $(".guess-you-like-li");
+ajaxProducts()
+	
+	function ajaxProducts(){
+		$.ajax({//获取json数据必写哦！！！
+			type:"get",
+			url:"json/lbyx.json",
+			dataType:"json",
+			success:function(data){//consol一下是很重要的！！！！！！
+				//假如一页获取m条
+				for(var i=0;i<data.homeList.length;i++){//加载json里面的图片，json里面数是从零开始的
+					 var html= "<ol class=\"main-list-ol\">";
+						 html+="<li class=\"main-list-ol-li1\"><img src=\""+data.homeList[i].img+"\"/></li>";
+						 html+="<li class=\"main-list-ol-li2\"><p>"+data.homeList[i].name+"</p><p>¥"+data.homeList[i].price+"</p></li>";
+						 html+="<li><i class=\"iconfont icon-gouwuche3 buy-car-icon\"></i></li></ol>";
+						ajaxLi.append(html);//通过不断地加载，但是点击之后ul又会清空来实现分页的效果
+				}
+				//首页点击，跳转页面，首页点击列表传参
+				$(".main-list-ol-li1,.main-list-ol-li2").tap(function  () {
+					 location.href = "product-details.html";//location.href实现客户端页面的跳转  	
+					 var homeNum= $(this).parent(".main-list-ol").index();
+					 window.sessionStorage.setItem("homeNum",homeNum);
+				});
+				//
+				$(".buy-car-icon").each(function () {
+					$(this).tap(function  () {
+						console.log("3")
+					});
+				});
+				
+			}
+			
+		});
+	}
 /*E index*/
  });
